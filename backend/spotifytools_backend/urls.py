ec5947e68e493103c0656540ffa336b84a8ad241
Rohan Patel <rohanblaze@hotmail.com>
2025-09-11 14:37:30 -0400

    last attempt at the docs endpoint

--- backend/spotifytools_backend/urls.py
+++ backend/spotifytools_backend/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 urlpatterns = [
     path("api/", include("api.urls")),
+        # OpenAPI schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
